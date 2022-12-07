854d1f37d7a212133754165b87f3e8041b9b862d
AndrzejSasak <andrzej.sasak01@gmail.com>
2022-12-07 16:32:51 +0100

    Add RandomTextMessageService

---
+++ src/main/java/com/example/RandomTextMessageService.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import java.util.List;
+import java.util.Random;
+
+public class RandomTextMessageService implements MessageService {
+
+    Random random = new Random();
+
+    private List<String> messages = List.of(
+            "msg0",
+            "msg1",
+            "msg2",
+            "msg3",
+            "msg4",
+            "msg5",
+            "msg6",
+            "msg7",
+            "msg8",
+            "msg9"
+    );
+
+    @Override
+    public String getMessage() {
+        int randomIndex = random.nextInt(10);
+        return messages.get(randomIndex);
+    }
+}
